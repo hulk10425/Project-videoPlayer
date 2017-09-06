1e06a0f154a947de9ae81ce5d6999f6268815491
hulk10425 <hulk104255@gmail.com>
2017-09-06 15:06:20 +0800

    video finally can run

--- videoPlayer/AVPlayerViewController.swift
+++ videoPlayer/AVPlayerViewController.swift
@@ -13,8 +13,9 @@ import AVFoundation
 class AVPlayerViewController: UIViewController {
     
     
-    
-    var playerLayer: AVPlayerLayer?
+    let filePath = "http://techslides.com/demos/sample-videos/small.mp4"
+    let videoURL =  URL(string: "http://techslides.com/demos/sample-videos/small.mp4")!
+    //var playerLayer: AVPlayerLayer?
     
     var playerItem:AVPlayerItem!
     var avplayer:AVPlayer!
@@ -22,6 +23,30 @@ class AVPlayerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.white
+        let txtField: UITextField = UITextField(frame: CGRect(x: 0, y: 0, width: 500.00, height: 30.00))
+        self.view.addSubview(txtField)
+        self.view.bringSubview(toFront: txtField)
+        //設定txtField
+        
+//        txtField.borderStyle = UITextBorderStyle.Line
+//        txtField.text = "myString"
+//        txtField.backgroundColor = UIColor.redColor()
+        self.view.addSubview(txtField)
+
+        //let videoURL = URL(fileURLWithPath: filePath)
+        
+        let player = AVPlayer(url: videoURL)
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        playerLayer.backgroundColor = UIColor.blue.cgColor
+       
+        
+        self.view.layer.addSublayer(playerLayer)
+        
+        
+        player.play()
         
 
         // Do any additional setup after loading the view.
